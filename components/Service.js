66725a52e1e9601c91622a908f6e5c59b8697ba8
Shubham <shubhamguptakrj2008@gmail.com>
2023-05-15 10:14:23 +0530

    Bug fix

--- components/Service.js
+++ components/Service.js
@@ -5,7 +5,7 @@ const Service = () => {
   return (
       <section
           id='service'
-      className={`w-screen max-md:max-h-max max-md:py-10 md:max-h-screen bg-[url('/rec1.png')] bg-cover bg-no-repeat bg-bottom flex-col flex items-center`}
+      className={`w-screen h-max max-md:py-10 md:h-screen bg-[url('/rec1.png')] bg-cover bg-no-repeat bg-bottom flex-col flex items-center md:justify-center`}
     >
           <h1 className="text-7xl md:text-[11rem] max-md:mb-20 text-center  text-[#FFB500] ">
               SERVICE
